a8b8d738f236c63058fcc8a0cdbcd5402efaea4b
Junaid Ahmed <junaid@Junaids-MacBook-Pro.local>
2020-09-27 20:59:49 -0400

    added styling

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,17 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                 children: <Widget>[
                   Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                     child: Text(
                       tx.amount.toString(),
                     ),
@@ -60,7 +71,9 @@ class MyHomePage extends StatelessWidget {
                   Column(
                     children: <Widget>[
                       Text(tx.title),
-                      Text(tx.date.toIso8601String()),
+                      Text(
+                        tx.date.toString(),
+                      ),
                     ],
                   )
                 ],
