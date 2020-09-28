faf4a79d2aa5b7c93dfe38161c20098583ae3ed7
Juned Ahmed <ahmedmohammedjuned@gmail.com>
2020-09-27 22:34:00 -0400

    store the inout on change

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,10 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  //String titleInput;
+  //String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -57,12 +61,21 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (value) {
+                    //  titleInput = value;
+                    //},
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    //onChanged: (val) => amountInput = val,
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                    },
                     textColor: Colors.purple,
                     child: Text('Add Transaction'),
                   )
